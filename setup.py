6e66bb3106ff5bc023980fefbeba77b3d0e5af8a
Davide De Tommaso <dtmdvd@gmail.com>
2019-05-30 08:52:39 +0200

    Network discovery on Windows fixed

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tobiiglassesctrl',
-    version='2.2.0',
+    version='2.2.1',
     description='A Python controller for Tobii Pro Glasses 2',
     url='https://github.com/ddetommaso/tobiiglasses-controller',
-    download_url='https://github.com/ddetommaso/tobiiglasses-controller/archive/2.2.0.tar.gz',
+    download_url='https://github.com/ddetommaso/tobiiglasses-controller/archive/2.2.1.tar.gz',
     install_requires=['netifaces'],
     author='Davide De Tommaso',
     author_email='ing.davidedetommaso@gmail.com',
